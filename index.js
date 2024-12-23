2b78c086188d99c49518c509a0966fa98fd0e750
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-23 20:18:09 +0200

    Add post user registration route example

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const yup = require('yup');
+
+const REGISTRATION_SCHEMA = yup.object({
+  email: yup.string().email().required(),
+  password: yup
+    .string()
+    .matches(/^.{8,32}$/, 'enter valid password')
+    .required(),
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  age: yup.number(),
+});
 
 // app - екземпляр серверу експресса
 const app = express();
 
 const users = [
-  { id: 0 , email: 'email1@gmail.com', password: '12345admin'},
-  { id: 1 , email: 'email2@gmail.com', password: 'sadsadsa'},
-  { id: 2 , email: 'email3@gmail.com', password: '423rfdf42d'},
-  { id: 3 , email: 'email4@gmail.com', password: 'dsac4354f'},
-  { id: 4 , email: 'email5@gmail.com', password: 'd4354fef'},
+  { id: 0, email: 'email1@gmail.com', password: '12345admin' },
+  { id: 1, email: 'email2@gmail.com', password: 'sadsadsa' },
+  { id: 2, email: 'email3@gmail.com', password: '423rfdf42d' },
+  { id: 3, email: 'email4@gmail.com', password: 'dsac4354f' },
+  { id: 4, email: 'email5@gmail.com', password: 'd4354fef' },
 ];
 
 // app містить функції для побудови маршрутів на сервері
@@ -29,9 +41,9 @@ app.get(
   // проміжний обробник запиту (міддлвер)
   (req, res, next) => {
     console.log('callback 1');
-    req.data = { id: 5};
+    req.data = { id: 5 };
     // каже що міддлвер зробив своє діло і можна викликати наступний обробник у ланцюжку
-    next(); 
+    next();
   },
   // проміжний обробник запиту (міддлвер)
   (req, res, next) => {
@@ -44,7 +56,7 @@ app.get(
     console.log('callback 3');
 
     const isRequestValid = Math.random() > 0.5;
-    if(isRequestValid) {
+    if (isRequestValid) {
       next();
     } else {
       res.send('Invalid data');
@@ -61,6 +73,47 @@ app.get(
 app.get('/users', (request, response) => {
   response.send(users);
 });
+
+// реєстрація користувача
+/*
+  1. отримати дані користувача з запиту
+  2. перевіріти дані на валідність
+  3. зберігти діні (реально їх записують у БД)
+  4. створити сессію для користувача
+  5. підготувати дані юзера до відправки
+  6. відправити дані у якості відповіді
+*/
+
+// міддлвер який отримає json дані з запиту
+const bodyParser = express.json();
+
+app.post(
+  '/users',
+  bodyParser,
+  (req, res, next) => {
+    console.log(req.body); // дані з тіла запиту
+
+    REGISTRATION_SCHEMA.validate(req.body)
+      .then((user) => {
+        req.user = user;
+        next();
+      })
+      .catch((error) => {
+        res.send(error.message);
+      });
+  },
+  (req, res, next) => {
+    const newUser = { ...req.user };
+
+    newUser.id = Date.now();
+
+    users.push(newUser);
+
+    const { password, ...preparedUser } = newUser;
+
+    res.send(preparedUser);
+  }
+);
 
 const PORT = 5000;
 
