3c9646ba3dc334cd8d01535ddfb6c1128142321c
Kyrylo <fresh.js.ftd.@gmail.com>
2024-12-24 20:15:42 +0200

    Refactor user controller to use User model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,21 @@
-let users = [
-  { id: 0, email: 'email1@gmail.com', password: '12345admin' },
-  { id: 1, email: 'email2@gmail.com', password: 'sadsadsa' },
-  { id: 2, email: 'email3@gmail.com', password: '423rfdf42d' },
-  { id: 3, email: 'email4@gmail.com', password: 'dsac4354f' },
-  { id: 4, email: 'email5@gmail.com', password: 'd4354fef' },
-];
+const User = require('../models/User');
 
 module.exports.getUsers = async (request, response) => {
+  const users = await User.findAll();
   response.send(users);
 };
 
 module.exports.getUser = async (req, res) => {
   const { params: { userId }} = req;
 
-  const user = users.find((user) => user.id === +userId);
+  const user = await User.findOne(+userId);
   res.send(user);
 }
 
 module.exports.createUser = async (req, res, next) => {
-  const newUser = { ...req.user };
+  const { user } = req;
 
-  newUser.id = Date.now();
-
-  users.push(newUser);
+  const newUser = await User.create(user);
 
   const { password, ...preparedUser } = newUser;
 
@@ -32,38 +25,21 @@ module.exports.createUser = async (req, res, next) => {
 module.exports.deleteUser = async (req, res) => {
   const {params: {userId}} = req;
 
-  const user = users.find((user) => user.id === +userId);
-
-  if(user) {
-    users = users.filter((user) => user.id !== +userId);
-    res.send(user);
-  } else {
-    res.send('User not exist');
+  try {
+    const deletedUser = await User.remove(+userId);
+    res.send(deletedUser);
+  } catch (error) {
+    res.send(error.message);
   }
 }
 
 module.exports.updateUser = async (req, res) => {
   const { user, params: {userId}} = req;
 
-  const foundUser = users.find((user) => user.id === +userId);
-
-  if(foundUser) {
-    let updatedUser;
-    users = users.map((userInDb) => {
-      if(userInDb.id === +userId) {
-        updatedUser = {
-          ...userInDb,
-          ...user
-        }
-
-        return updatedUser;
-      }
-
-      return userInDb;
-    });
-
+  try {
+    const updatedUser = await User.updateOne(+userId, user);
     res.send(updatedUser);
-  } else {
-    res.send('User not exist');
+  } catch (error) {
+    res.send(error.message);
   }
 }
